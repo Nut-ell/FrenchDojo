b3adfdc4f5eacabb469d9a00b7b97c28d58795f9
Nut-ell <nutowll88888888@google.com>
2025-12-04 21:15:54 +0900

    Update audio extension to mp3 and adjust App code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ErrorBoundary } from './ErrorBoundary';
-import './index.css';
 
 // Add global error handler
 window.addEventListener('error', (event) => {
